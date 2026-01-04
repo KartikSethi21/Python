efe7a4026363f36e1f84fab30b500f8510481115
KartikSethi21 <kartiksethi003@gmail.com>
2026-01-04 23:45:33 +0530

    code files

---
+++ 4Fourth/data_type.py
@@ -0,0 +1,12 @@
+username = 'kartik'
+print("Length of username is ",len(username))
+
+print("First Letter ",username[0])
+
+# username[0]= "J"
+# TypeError: 'str' object does not support item assignment
+
+
+print("First Letter ",username)
+print("Last Letter ",username[-1])
+print("Second Letter ",username[0])
